72f099c04292c494e8f383de294e9523d42d42c4
Flower Aviles <itsfloweraviles@gmail.com>
2021-08-08 02:15:20 -0400

    completed backend

---
+++ back-end/models/transaction.js
@@ -0,0 +1,232 @@
+module.exports = [
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1200",
+        from: "Work",
+    },
+    
+    {
+        date: "June 4",
+        name: "Uber", 
+        amount: "20",
+        from: "Main Account",
+    },
+
+    {
+        date: "June 4",
+        name: "Uber", 
+        amount: "22",
+        from: "Main Account",
+    },
+
+    {
+        date: "June 4",
+        name: "Savings", 
+        amount: "1000",
+        from: "Saving Account",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+
+    {
+        date: "June 4",
+        name: "Income", 
+        amount: "1900",
+        from: "Work",
+    },
+]
